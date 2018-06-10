e604e6d5042780d966b3c0622639e04af455cddf
nihavend <serkan.tas@likyateknoloji.com>
2018-06-10 11:31:45 +0300

    initial commit

--- MXRoyaleUtils/src/com/likya/royale/CompareUtil.java
+++ MXRoyaleUtils/src/com/likya/royale/CompareUtil.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class GetProjectFiles {
+public class CompareUtil {
 	
 	public static void main(String[] args) throws IOException {
 
